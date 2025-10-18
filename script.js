411488bd519aff33c9cd2105c19b3f7f8bb5eccd
TheDavidLaCosa <64459822+TheDavidLaCosa@users.noreply.github.com>
2025-10-18 16:42:42 +0200

    Improved

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 /* Aquí pots afegir codi JavaScript si el necessites */
 console.log("La pàgina principal s'ha carregat correctament.");
+
+// --- Zoom Poster ---
+const modal = document.getElementById("zoomModal");
+const poster = document.getElementById("posterImage");
+const zoomed = document.getElementById("zoomedImage");
+const closeBtn = document.querySelector(".close-zoom");
+
+poster.onclick = function() {
+  modal.style.display = "flex";
+  zoomed.src = this.src;
+};
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+};
+
+// També permet tancar fent clic fora de la imatge
+modal.onclick = function(e) {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
